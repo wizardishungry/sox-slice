c98b57da8073f1f55c851be70aea0ec887597149
Jon Williams <jonathan.williams@gmail.com>
2010-11-04 20:07:55 -0400

    calculate frequencies

--- sox-slice.rb
+++ sox-slice.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/env ruby
 
 NUM_SLICES = 10
+LOW_FREQ = 32
+GAIN = 6 # dB
+
 class Slicer
     def usage
         puts "give me a list of wav (or sox compatible files on the commandline"
     end
-    def slice(file,slices)
+    def slice(file,slices,low_freq)
         @file=file
         @slices=slices
-        slices=1..slices
+        slices=0..slices-1
+        @freqs=slices.map{|x| low_freq*(2**x)}
+
         if !File.exist? file
             raise "File doesn't exist (#{file})"
         end
@@ -28,7 +33,7 @@ class Slicer
     protected
 
     def do_slice(num)
-        puts "Slice #{num} of #{@slices}"
+        puts "Slice #{num+1} of #{@slices}, #{@freqs[num]} hz"
     end
 end
 
@@ -38,6 +43,6 @@ if ARGV.empty?
     slicer.usage
 else
     ARGV.each do|a|
-        slicer.slice a, NUM_SLICES
+        slicer.slice a, NUM_SLICES, LOW_FREQ
     end
 end
